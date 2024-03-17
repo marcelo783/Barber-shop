a201e13ca50c081bc5e916f3198c7b75f729e5b8
marcelo783 <marcelo.oliveira7931@gmail.com>
2024-03-16 23:54:28 -0300

    seção Quem Somos

---
+++ barber-shop/src/components/information/info.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Container, Sections,  } from '../../styles/common.style';
+import { H1, Para } from "../../styles/Typography.style";
+import { InfoWrapper, InfoText, InfoImage } from "./info.stlyle";
+import Infoimg from "../../imagens/FOTO BARBEIRO.jpg";
+const Info = () => {
+
+    return (
+
+        <div>
+            <Container>
+                <Sections>
+                    <H1>Quem Somos</H1>
+                    <InfoWrapper>
+                    <InfoImage>
+                        <img src={Infoimg} alt="info" />
+                    </InfoImage>
+                    <InfoText>
+                        <Para>Bem-vindo à Barbearia Feke, o ponto de encontro entre tradição e modernidade no universo do cuidado masculino. Desde nossa fundação em [Ano de Fundação] pelo visionário [Nome do Fundador], nos dedicamos a transformar cada visita em uma experiência única.
+
+                        Nossa equipe, composta por especialistas em cortes clássicos e estilos contemporâneos, está empenhada em oferecer não apenas um serviço excepcional, mas também um momento de pausa e renovação. Aqui, cada detalhe, da decoração ao atendimento personalizado, é pensado para garantir que você saia se sentindo renovado, por dentro e por fora.
+                        Na Barbearia Feke, acreditamos no valor do autocuidado masculino como um ritual. Nosso ambiente é desenhado para ser seu refúgio de tranquilidade e estilo, um local onde a qualidade e a tradição se encontram para criar uma experiência sem igual.
+                        </Para>
+                    </InfoText>
+                </InfoWrapper>
+                </Sections>
+            </Container>
+        </div>
+    )
+
+
+};
+
+export default Info;
---
+++ barber-shop/src/components/information/info.stlyle.jsx
@@ -0,0 +1,30 @@
+import  styled  from "styled-components";
+
+
+export const InfoWrapper = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 2rem 0;
+
+
+`;
+
+export const InfoImage = styled.div`
+
+    border: 5px solid var(--Gold);
+    img {
+        width: 100vh;
+        height: 76vh;
+        max-width: 70vh; 
+    }
+
+
+`;
+
+export const InfoText = styled.div`
+    flex: 1;
+    padding: 0 2rem;
+
+
+`;
